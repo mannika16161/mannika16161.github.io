668a68f917f20701478cabf02e23326c4a25ea4a
mannika16161 <mannika16161@iiitd.ac.in>
2021-01-09 17:34:02 +0530

    added node modules

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Mannika Garg</li>
+              <li>Mannika Garg</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
